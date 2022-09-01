99151c21ac16daf6a38c0ef8478c75628579c4f2
harimbu <harimbu@gmail.com>
2022-09-01 23:02:09 +0900

    google ads

--- lib/binding/app_binding.dart
+++ lib/binding/app_binding.dart
@@ -1,4 +1,3 @@
-import 'package:english_word_800/controller.dart/ads_eng_controller.dart';
 import 'package:english_word_800/controller.dart/ads_home_controller.dart';
 import 'package:english_word_800/controller.dart/app_controller.dart';
 import 'package:get/get.dart';
@@ -7,7 +6,6 @@ class AppBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(AppController());
-    Get.put(AdsEngController());
     Get.put(AdsHomeController());
   }
 }
